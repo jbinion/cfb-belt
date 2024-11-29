f2aa94df2e374dd348be7b2f391642506ace0bc7
jbinion <jbinion0366@gmail.com>
2024-11-28 22:31:30 -0500

    general dev

--- svelte/src/routes/+page.ts
+++ svelte/src/routes/+page.ts
@@ -2,11 +2,18 @@
 // it so that it gets served as a static asset in production
 export const prerender = true;
 import type { PageLoad } from './$types';
-import data from './old.lineage.json';
+import { data } from './final.json';
 export const load: PageLoad = async () => {
 	const lastTen = data.reverse().slice(0, 10);
+	const teams = lastTen.map((x) => ({
+		name: x.name,
+		color: x.color,
+		alternatColor: x.alternateColor,
+		logo: x.logo,
+		key: x.games[0].start_date
+	}));
 	return {
-		current: lastTen[0],
-		recent: lastTen.slice(1)
+		current: teams[0],
+		recent: teams.slice(1)
 	};
 };
