12250edd7c0de4300a047fdfb12f6fc956e789cf
jbinion <jbinion0366@gmail.com>
2024-12-02 14:05:22 -0500

    Update TeamCardFull.svelte

--- svelte/src/components/TeamCardFull.svelte
+++ svelte/src/components/TeamCardFull.svelte
@@ -8,16 +8,23 @@
 	// figure out who team won from and who lost too
 </script>
 
-<div class="flex items-center space-x-4">
-	<img src={logo} class="h-12" alt={`${name} logo`} />
-	<div>
-		<a class="text-black" {href}>{name}</a>
-		<div class="flex flex-row space-x-4">
-			<p class="text-sm">{games.length - 2} Defenses</p>
-			<p class="text-sm">
-				{days} days {new Date(games[0].start_date).toLocaleDateString()} -
-				{new Date(games.at(-1).start_date).toLocaleDateString()}
-			</p>
-		</div>
+<a class=" text-black" {href}>
+	<div class="flex items-center hover:bg-gray-100">
+		<img src={logo} class="mr-6 h-12" alt={`${name} logo`} />
+
+		<p class=" w-48">{name}</p>
+		<p class="w-24 text-sm">{games.length - 2} Defenses</p>
+		<p class="ml-auto font-mono text-sm font-thin">
+			{new Date(games[0].start_date).toLocaleDateString('default', {
+				month: 'long',
+				year: 'numeric',
+				day: 'numeric'
+			})} -
+			{new Date(games.at(-1).start_date).toLocaleDateString('default', {
+				month: 'long',
+				year: 'numeric',
+				day: 'numeric'
+			})}
+		</p>
 	</div>
-</div>
+</a>
