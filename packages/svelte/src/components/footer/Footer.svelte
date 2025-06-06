68113a806f7fa621f1b7ece8705ee88d55ddd2ef
jbinion <jbinion0366@gmail.com>
2025-06-06 12:58:22 -0400

    refactor: footer

--- packages/svelte/src/components/footer/Footer.svelte
+++ packages/svelte/src/components/footer/Footer.svelte
@@ -1,18 +1,32 @@
-<footer
-	class="content mb-4 mt-24 flex flex-col items-center justify-between md:flex-row md:space-x-4"
->
-	<div class="flex-cold flex space-x-4 md:flex-row md:space-x-6">
-		<a class="text-gray-600" href="/history"> Complete Lineage</a>
-		<a class="text-gray-600" href="/teams"> Teams</a>
+<script>
+	import { AiFillGithub } from 'svelte-icons-pack/ai';
+	import { Icon } from 'svelte-icons-pack';
+</script>
+
+<footer class="content mb-4 mt-24 space-y-4">
+	<div class="flex flex-row justify-between">
+		<div class="flex-cold flex items-center space-x-4 md:flex-row md:space-x-6">
+			<a class="text-gray-600 hover:text-black" href="/history"> Complete Lineage</a>
+			<a class="text-gray-600 hover:text-black" href="/teams"> Teams</a>
+		</div>
+		<div class="flex-cold flex items-center space-x-4 md:flex-row md:space-x-6">
+			<a href="mailto:contact@cfb-belt.com" class="text-sm text-gray-600 hover:text-black"
+				>contact@cfb-belt.com</a
+			>
+
+			<a
+				class="cursor-pointer text-gray-600 hover:text-black"
+				href="https://github.com/jbinion/cfb-belt"
+				target="_blank"
+			>
+				<Icon src={AiFillGithub} size={'32px'} />
+			</a>
+		</div>
 	</div>
-	<!-- <p class="text-md font-mono font-semibold">[contact]</p>
-	<a href="mailto:contact@cfb-belt.com" class="text-md">contact@cfb-belt.com</a>
-	<p>
-		Data for this site comes from <a href="https://collegefootballdata.com"
+
+	<p class=" text-xs text-gray-600">
+		Data for this site comes from <a href="https://collegefootballdata.com" target="_blank"
 			>collegefootballdata.com</a
 		>
-	</p> -->
-	<div class="text-xs text-gray-500">
-		© {new Date().getFullYear()} CFB-Belt.com
-	</div>
+	</p>
 </footer>
