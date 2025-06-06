e2932566bf9012ab0de036e25274948f077c1cc4
jbinion <jbinion0366@gmail.com>
2025-06-06 15:27:29 -0400

    fix: multiple games per 'week' handling

--- packages/data/src/beltTracker.js
+++ packages/data/src/beltTracker.js
@@ -21,11 +21,22 @@ class BeltTracker {
       console.log(game);
       throw new Error('Invalid game: ' + game);
     }
+    // handle refecthing current week when belt holder losses
+    if (this.reigns.at(-1)?.games.at(-1).id === game.id) return;
+
     this.teams.add(game.home_team);
     this.teams.add(game.away_team);
 
     const winner = getGameWinner(game, this.currentHolder);
     this.currentHolder = winner;
+    if (this.reigns.length === 0) {
+      this.reigns.push({
+        team: winner,
+        games: [{ ...game, type: 'win' }],
+        startDate: game.start_date,
+      });
+      return;
+    }
     if (winner === this.reigns.at(-1).team) {
       this.reigns.at(-1).games.push({ ...game, type: 'defense' });
       return;
@@ -41,7 +52,7 @@ class BeltTracker {
 
 export default BeltTracker;
 
-const getGameWinner = (game, currentHolder) => {
+export const getGameWinner = (game, currentHolder) => {
   if (game.home_points === game.away_points) return currentHolder;
   return game.home_points > game.away_points ? game.home_team : game.away_team;
 };
--- packages/data/src/crawler.js
+++ packages/data/src/crawler.js
@@ -1,37 +1,49 @@
 import getGame from './api/getGame';
 import getWeeks from './api/getWeeks';
-import BeltTracker from './beltTracker';
+import BeltTracker, { getGameWinner } from './beltTracker';
 import delay from './utils/delay';
 
-const crawler = async ({ team, startYear, maxYear }) => {
+const crawler = async ({ team, startYear, maxYear, startWeekIndex = 0 }) => {
   console.log(`creating lineage for team ${team} from year ${startYear}`);
   const beltTracker = BeltTracker.getInstance(team);
   let active = true;
   let year = startYear;
+  // used for testing
+  let initialStart = startWeekIndex;
 
   while (active) {
     const weeks = await getWeeks(year);
-    for (let i = 0; i < weeks.length; i++) {
+    console.log(weeks);
+    for (let i = initialStart; i < weeks.length; ) {
       await delay(400);
+      const initalHolder = beltTracker.currentHolder || null;
       const games = await getGame({
         year,
         week: weeks[i].week,
         team: beltTracker.currentHolder || null,
         type: weeks[i].type,
       });
-      if (!games) continue;
       console.log(games);
-      if (games.length === 1) {
-        beltTracker.addGame(games[0]);
+      if (!games || games.length == 0) {
+        i++;
         continue;
       }
-      // a team can play more than one game per 'week'
-      // if the current belt holder loses the first game,
-      // we need to refetch the week for the new belt holder to get the second game, likely a championship
+      console.log(games);
       if (games) {
         games.forEach((game) => beltTracker.addGame(game));
       }
+      // a team can play more than one game per 'week'
+      // if the current belt holder loses the first game,
+      // we need to refetch the week for the new belt holder to get potential second games, likely a championship
+      // this assumes there will never be more than 2 games in a week.
+      let firstGameWinner = getGameWinner(games[0], initalHolder);
+      if (firstGameWinner === initalHolder) {
+        i++;
+      }
     }
+
+    //reset testing value which is meant to start crawler at season x week y
+    initialStart = 0;
     year++;
     if (year >= maxYear) active = false;
   }
