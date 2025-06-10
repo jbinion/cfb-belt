375963736d2b171f12c3f98699c712be374c37ec
jbinion <jbinion0366@gmail.com>
2025-06-10 02:32:33 -0400

    fix: return correct name

--- packages/data/src/populateTeamData.js
+++ packages/data/src/populateTeamData.js
@@ -2,16 +2,16 @@ import getTeamsFromEspn from './api/getTeamsFromEspn.js';
 import normalizeTeamNames from './utils/normalizeTeamNames.js';
 
 const populateTeamData = async (teamArray) => {
-  const normalizedTeamNames = teamArray.map((x) => normalizeTeamNames(x));
-
   const espnTeamData = await getTeamsFromEspn();
 
   const noData = [];
 
-  const teamData = normalizedTeamNames.map((team) => {
+  const teamData = teamArray.map((team) => {
     const espnTeam = espnTeamData.find(
       (espnTeam) => espnTeam.name === normalizeTeamNames(team)
     );
+    console.log(espnTeam);
+    console.log(team);
     if (espnTeam) return { ...espnTeam, name: team };
     noData.push(team);
     return { name: team };
