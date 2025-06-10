fe33930430980b86c8fc5d27eb4d87dabd0d8fe6
jbinion <jbinion0366@gmail.com>
2025-06-10 02:33:15 -0400

    feat: initial next game data from db

--- packages/svelte/src/components/NextGame.svelte
+++ packages/svelte/src/components/NextGame.svelte
@@ -1,17 +1,37 @@
+<script lang="ts">
+	export let date = '';
+	export let homeTeam = '';
+	export let awayTeam = '';
+	export let homeLogo = '';
+	export let awayLogo = '';
+</script>
+
 <div>
 	<p class="sectionTitle !mb-2">Next Game</p>
 	<div
-		class="card flex flex-row items-center justify-between bg-[var(--color-bg-light)] p-4 shadow-sm"
+		class="card flex flex-col items-center justify-between bg-[var(--color-bg-light)] p-4 shadow-sm"
 	>
-		<p class="font-mono text-sm">Saturday, August 30, 2025</p>
-		<div class="flex flex-row items-center">
+		<div class="flex w-full flex-row items-center">
+			<div class="flex flex-1 flex-row items-center">
+				<img
+					src={`/webp/large/${homeLogo}.webp`}
+					class=" h-[60px] w-[60px] rounded-full border-2 bg-[var(--color-white)]"
+					alt="LIU logo"
+				/>
+				<p class="ml-2 font-bold text-[var(--color-primary)]">{homeTeam}</p>
+			</div>
+
 			<span class="mx-2 font-mono font-bold text-[var(--color-orange)]">vs</span>
-			<img
-				src={`/webp/small/LIUl.webp`}
-				class="border-accent h-[60px] w-[60px] rounded-full border-2 bg-[var(--color-white)]"
-				alt="LIU logo"
-			/>
-			<p class="ml-2 text-center font-bold text-[var(--color-primary)]">LIU</p>
+			<div class="flex flex-1 flex-row items-center">
+				<p class="ml-2 font-bold text-[var(--color-primary)]">{awayTeam}</p>
+
+				<img
+					src={`/webp/large/${awayLogo}.webp`}
+					class=" h-[60px] w-[60px] rounded-full border-2 bg-[var(--color-white)]"
+					alt="LIU logo"
+				/>
+			</div>
 		</div>
+		<p class="font-mono text-sm">Saturday, August 30, 2025</p>
 	</div>
 </div>
