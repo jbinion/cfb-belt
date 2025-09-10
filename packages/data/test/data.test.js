035960a1cab4ca62692d89bc9fe3c9f149613295
jbinion <jbinion0366@gmail.com>
2025-09-10 16:10:26 -0400

    chore: fix test

--- packages/data/test/data.test.js
+++ packages/data/test/data.test.js
@@ -1,9 +1,6 @@
 import { expect, test } from 'vitest';
 import getWeeks from '../src/api/getWeeks';
 import getGame from '../src/api/getGame';
-test('first test', () => {
-  expect(1).toBe(1);
-});
 
 test('get weeks', async () => {
   const weeks = await getWeeks(2002);
@@ -19,5 +16,18 @@ test('get game', async () => {
     type: 'regular',
   });
   console.log(game);
-  expect(game).toEqual({ test: 'e' });
+  expect(game).toEqual([
+    {
+      away_points: 0,
+      away_team: 'Long Island University',
+      completed: true,
+      home_points: 55,
+      home_team: 'Florida',
+      id: 401752668,
+      start_date: '2025-08-30T23:00:00.000Z',
+      type: 'regular',
+      week: 1,
+      year: 2025,
+    },
+  ]);
 });
