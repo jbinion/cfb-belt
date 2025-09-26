53a8c61d982c33d53038382c7730c7b380b38475
jbinion <jbinion0366@gmail.com>
2025-09-26 00:24:08 -0400

    Update TeamCardFull.svelte

--- packages/svelte/src/components/TeamCardFull.svelte
+++ packages/svelte/src/components/TeamCardFull.svelte
@@ -1,4 +1,6 @@
 <script>
+	import formatDate from "$lib/formatDate";
+
 	export let name = '';
 
 	export let logo = '';
@@ -8,15 +10,15 @@
 	const href = `/teams/${slug}`;
 </script>
 
-<a class="hover:bg-card-hover text-foreground flex items-center py-2" {href}>
+<a class="hover:bg-card-hover text-foreground flex items-center py-1" {href}>
+	<p class="w-32 text-start text-sm">{formatDate(startDate)}</p>
 	<img src={`/webp/large/${logo}.webp`} class="mr-6 h-12 w-12" alt={`${name} logo`} />
-	<h2 class="flex-1">{name}</h2>
+	<h2 class="flex-1">{name} </h2>
 
-	<div class="text-foreground-muted text-fore flex flex-row space-x-4 text-sm">
-		<p class=" text-end font-mono">
+		<p class=" text-end text-foreground-muted  text-sm">
 			{defendCount}
 			{defendCount === 1 ? 'Defense' : 'Defenses'}
 		</p>
-		<p class="w-24 text-end font-mono">{new Date(startDate).toLocaleDateString('default')}</p>
-	</div>
+		
+
 </a>
