f763bcc98192c7d22f71c7f6e9ce095b2b1b0888
jbinion <jbinion0366@gmail.com>
2025-10-02 00:17:19 -0400

    Update Navbar.svelte

--- packages/svelte/src/components/Navbar.svelte
+++ packages/svelte/src/components/Navbar.svelte
@@ -7,14 +7,13 @@
 	<a
 		href="/"
 		class="flex items-center border-b-2 border-transparent text-sm font-bold tracking-wide"
-		class:border-black={currentPath === '/'}
 	>
 		CFB Belt
 	</a>
 	<a
 		href="/history"
 		class=" border-b-2 border-transparent text-sm font-semibold transition-colors"
-		class:border-black={currentPath.startsWith('/history')}>Lineage</a
+		class:!border-black={currentPath.startsWith('/history')}>Lineage</a
 	>
 	<a
 		href="/teams"
