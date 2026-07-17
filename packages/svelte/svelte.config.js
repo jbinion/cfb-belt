eb6193e1385b6f8cc9f6411c277166470d391e9b
jbinion <jbinion@pm.me>
2026-07-17 02:26:42 -0400

    build: svelte adapter static

--- packages/svelte/svelte.config.js
+++ packages/svelte/svelte.config.js
@@ -1,6 +1,4 @@
-// import adapter from '@sveltejs/adapter-static';
-
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -11,16 +9,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter: adapter({
-		// 	// default options are shown. On some platforms
-		// 	// these options are set automatically — see below
-		// 	pages: 'build',
-		// 	assets: 'build',
-		// 	fallback: undefined,
-		// 	precompress: false,
-		// 	strict: true
-		// })
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true,
+		}),
 	},
 };
 
