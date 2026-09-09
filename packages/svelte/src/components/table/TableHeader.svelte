3fdcac220744b0305917150af4bcd3fc1905619c
jbinion <jbinion@pm.me>
2026-09-08 22:22:50 -0400

    inital

---
+++ packages/svelte/src/components/table/TableHeader.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface Props {
+		title: string;
+		textEnd?: boolean;
+	}
+
+	let { title, textEnd = false }: Props = $props();
+</script>
+
+<th
+	class={`px-4 py-3 text-xs font-semibold uppercase tracking-wide text-foreground-muted ${textEnd ? 'text-end' : 'text-left'}`}
+	>{title}</th
+>
